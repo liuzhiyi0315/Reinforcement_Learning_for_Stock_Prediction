b3ed5ea26ad88a9d4d18f19ad29f2c9db4237b9f
liuzhiyi <liuzhiyi@navinfo.com>
2019-04-03 10:22:31 +0800

    add PercentagePrinter

--- functions.py
+++ functions.py
@@ -1,30 +1,61 @@
 import numpy as np
 import math
 
+
 # prints formatted price
 def formatPrice(n):
-	return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
+    return ("-$" if n < 0 else "$") + "{0:.2f}".format(abs(n))
+
 
 # returns the vector containing stock data from a fixed file
 def getStockDataVec(key):
-	vec = []
-	lines = open("data/" + key + ".csv", "r").read().splitlines()
+    vec = []
+    lines = open("data/" + key + ".csv", "r").read().splitlines()
 
-	for line in lines[1:]:
-		vec.append(float(line.split(",")[4]))
+    for line in lines[1:]:
+        vec.append(float(line.split(",")[4]))
 
-	return vec
+    return vec
+
 
 # returns the sigmoid
 def sigmoid(x):
-	return 1 / (1 + math.exp(-x))
+    return 1 / (1 + math.exp(-x))
+
 
 # returns an an n-day state representation ending at time t
 def getState(data, t, n):
-	d = t - n + 1
-	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
-	res = []
-	for i in range(n - 1):
-		res.append(sigmoid(block[i + 1] - block[i]))
+    d = t - n + 1
+    block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1]  # pad with t0
+    res = []
+    for i in range(n - 1):
+        res.append(sigmoid(block[i + 1] - block[i]))
 
-	return np.array([res])
+    return np.array([res])
+
+
+class PercentagePrinter:
+    def __init__(self, text, num_all, blocks=50):
+        self.text = text
+        self.num_all = num_all
+        self.blocks = blocks
+
+    def print(self, index, state_text=None):
+        percentage = int(100 * index / self.num_all)
+        if state_text is not None:
+            print("\r{} {} {}% ".format(self.text, state_text, percentage), end='')
+        else:
+            print("\r{} {}% ".format(self.text, percentage), end='')
+        _percentage = int(percentage / (100 / self.blocks))
+        for p in range(0, _percentage):
+            print('🀫', end='')
+        for p in range(self.blocks - _percentage):
+            print('🀆', end='')
+        print("", end='', flush=True)
+
+    def final_print(self, state_text=None):
+        if state_text is not None:
+            print("\r{} {} {}% ".format(self.text, state_text, 100), end='')
+        else:
+            print("\r{} {}% ".format(self.text, 100), end='')
+        print("")
--- train.py
+++ train.py
@@ -2,11 +2,12 @@ from agent.agent import Agent
 from functions import *
 import sys
 
-if len(sys.argv) != 4:
-	print("Usage: python train.py [stock] [window] [episodes]")
-	exit()
+# if len(sys.argv) != 4:
+# 	print("Usage: python train.py [stock] [window] [episodes]")
+# 	exit()
 
-stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+# stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+stock_name, window_size, episode_count = '^GSPC', 20, 600
 
 agent = Agent(window_size)
 data = getStockDataVec(stock_name)
@@ -14,14 +15,18 @@ l = len(data) - 1
 batch_size = 32
 
 for e in range(episode_count + 1):
-	print("Episode " + str(e) + "/" + str(episode_count))
+	message = "Episode " + str(e) + "/" + str(episode_count)
 	state = getState(data, 0, window_size + 1)
 
 	total_profit = 0
 	agent.inventory = []
 
+	action_sum = {0: 0, 1: 0, 2: 0}
+
+	pp = PercentagePrinter(message, l)
 	for t in range(l):
 		action = agent.act(state)
+		action_sum[action] += 1
 
 		# sit
 		next_state = getState(data, t + 1, window_size + 1)
@@ -29,13 +34,15 @@ for e in range(episode_count + 1):
 
 		if action == 1: # buy
 			agent.inventory.append(data[t])
-			print("Buy: " + formatPrice(data[t]))
+			# print("Buy: " + formatPrice(data[t]))
 
 		elif action == 2 and len(agent.inventory) > 0: # sell
 			bought_price = agent.inventory.pop(0)
 			reward = max(data[t] - bought_price, 0)
 			total_profit += data[t] - bought_price
-			print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+			# print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+
+		pp.print(t)
 
 		done = True if t == l - 1 else False
 		agent.memory.append((state, action, reward, next_state, done))
@@ -44,10 +51,13 @@ for e in range(episode_count + 1):
 		if done:
 			print("--------------------------------")
 			print("Total Profit: " + formatPrice(total_profit))
+			print(f"buy {action_sum[1]}, sell {action_sum[2]}, hold {action_sum[0]}")
 			print("--------------------------------")
 
 		if len(agent.memory) > batch_size:
 			agent.expReplay(batch_size)
 
-	if e % 10 == 0:
-		agent.model.save("models/model_ep" + str(e))
+	pp.final_print()
+
+	# if e % 10 == 0:
+	# 	agent.model.save("models/model_ep" + str(e))
